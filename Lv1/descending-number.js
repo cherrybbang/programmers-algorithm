a9688a2c7db389909985e82d2704d5ea40c0453d
JiYu <eunjun1018@gmail.com>
2025-12-30 13:10:01 +0900

    정수 내림차순으로 배치하기

---
+++ Lv1/descending-number.js
@@ -0,0 +1,21 @@
+// 정수 내림차순으로 배치하기
+
+function solution(n) {
+    const num = String(n);
+    let newArr = Array.from(num);
+    newArr.sort((a, b) => b - a);
+    return Number(newArr.join(''));
+}
+
+console.log(solution(118372));
+
+
+// 다른 풀이
+// function solution(n) {
+//   return Number(
+//     String(n)
+//       .split("")
+//       .sort((a, b) => b - a)
+//       .join("")
+//   );
+// }
