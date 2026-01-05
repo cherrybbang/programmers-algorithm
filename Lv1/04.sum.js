7b4ce089b99818b2e50be3618270e51b876c12d2
JiYu <eunjun1018@gmail.com>
2026-01-05 16:04:47 +0900

    풀이과정 설명

--- Lv1/04.sum.js
+++ Lv1/04.sum.js
@@ -1,5 +1,9 @@
 // 두 정수 사이의 합
 
+// 해결 및 고민과정
+// 1. 매개변수 a와 b의 값을 비교하여 시작값(최소)과 끝값(최대) 구해서 기준을 잡았다.
+// 2. for문을 사용하여 시작값부터 끝값까지 반복하면서 sum 변수에 값을 누적시켰다.
+
 function solution(a, b) {
   let sum = 0;
   const start = Math.min(a, b);
@@ -11,5 +15,3 @@ function solution(a, b) {
 
   return sum;
 }
-
-// https://devchul.tistory.com/14
