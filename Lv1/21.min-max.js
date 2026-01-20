683204d337f4451f9141fb5d363769170f8450af
JiYu <eunjun1018@gmail.com>
2026-01-20 17:12:39 +0900

    '최대공약수와 최소공배수

---
+++ Lv1/21.min-max.js
@@ -0,0 +1,14 @@
+// 최대공약수와 최소공배수
+
+function solution(n, m) {
+    var answer = [];
+    let a = Math.min(n, m);
+    let b = Math.max(n, m);
+    
+    for(let i = a; i >= 1; i--) {
+        if(a % i == 0 && b % i == 0) {
+            return [i, (a * b / i)]
+        }
+    }
+    return answer;
+}
