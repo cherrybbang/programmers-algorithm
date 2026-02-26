d9804a9a315a78f961b984a83040aec7664fcca7
JiYu <eunjun1018@gmail.com>
2026-02-26 19:15:11 +0900

    예산

---
+++ Lv1/26.budget.js
@@ -0,0 +1,22 @@
+//  예산
+
+function solution(d, budget) {
+    // 1. 신청 금액이 작은 부서부터 정렬 => 예산을 최대한 많이 사용할 수 있도록
+    d.sort((a, b) => a - b);
+    
+    let count = 0;
+    let currentBudget = 0;
+    
+    // 2. 작은 금액부터 예산에 더하며 비교
+    for (let i = 0; i < d.length; i++) {
+        if (currentBudget + d[i] <= budget) {
+            currentBudget += d[i];
+            count++;
+        } else {
+            // 예산 초과 시 종료
+            break;
+        }
+    }
+    
+    return count;
+}
